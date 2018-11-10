9fe064e89962252d0b1e153ab3a97cc58b2a56ef
BuildTools <unconfigured@null.spigotmc.org>
2018-11-10 23:37:53 +0900

    dd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 #for korean
-source_encoding = 'cp949'
+#source_encoding = 'cp949'
 
 
 
@@ -124,7 +124,7 @@ htmlhelp_basename = 'pythonintemediatedoc'
 #\setCJKsansfont{MS Gothic}
 #'''
 
-latex_elements = {
+#latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
@@ -140,15 +140,15 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
-}
+#}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'pythonintemediate.tex', 'Python Intemediate Documentation',
-     'sean', 'manual'),
-]
+#latex_documents = [
+#    (master_doc, 'pythonintemediate.tex', 'Python Intemediate Documentation',
+#     'sean', 'manual'),
+#]
 
 
 # -- Options for manual page output ------------------------------------------
